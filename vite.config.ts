d25f54fda92374a88c884e72ce18af5d7aec1704
username <drewesk@gmail.com>
2025-07-28 11:20:41 -0400

    add sign-up card, css bug still in motion, crush soon

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
-import tsconfigPaths from 'vite-tsconfig-paths'
+import tailwindcss from '@tailwindcss/vite'
+import path from 'path'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    tsconfigPaths() // Enables @/* alias from tsconfig
+    tailwindcss(),
   ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src')
+    }
+  }
 })
